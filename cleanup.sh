d5357f4f377803a7a91d10010ebb147583a62513
Issam Mani <issamouu69@gmail.com>
2024-11-14 23:10:59 +0100

    fix: cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,8 @@
+source .env
 # Clean up all records
-curl -X DELETE ${SERVER}/buckets/main-workspace/collections/${COLLECTION_ID}/records -H "Authorization:${BEARER_TOKEN}"
+curl -X DELETE "${SERVER}/buckets/${BUCKET}/collections/${COLLECTION_ID}/records" -H "Authorization:${BEARER_TOKEN}"
+# Sign the changes
+curl -X PATCH ${SERVER}/buckets/main-workspace/collections/${COLLECTION_ID} \
+     -H 'Content-Type:application/json' \
+     -d '{"data": {"status": "to-review"}}' \
+     -H "Authorization:${BEARER_TOKEN}"
